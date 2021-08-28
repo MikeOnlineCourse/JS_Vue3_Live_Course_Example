4a4198562016d4c8f8cbddcb9d280cf375406938
Mike <a3804430@hotmail.com>
2021-08-28 21:52:41 +0800

    fix

--- 03-Vue_VS_React/vue3-login-page/src/composition-api/useRegistered.js
+++ 03-Vue_VS_React/vue3-login-page/src/composition-api/useRegistered.js
@@ -1,27 +1,28 @@
-import { ref, reactive, inject } from 'vue';
+import { ref, inject } from 'vue';
 import axios from 'axios';
+
+const REGISTER_URL = 'https://vue-lessons-api.herokuapp.com/auth/registered';
+
 export function useRegistered() {
   const { handleRegisterPage } = inject('mapStore');
   const isSubmit = ref(false);
-  const errorMessage = reactive({});
+  const errorMessage = ref({});
 
   const submitRegister = async registered => {
     if (isSubmit.value) return;
     isSubmit.value = true;
     try {
-      await axios.post('https://vue-lessons-api.herokuapp.com/auth/registered', registered);
-      isSubmit.value = false;
+      await axios.post(REGISTER_URL, registered);
       handleRegisterPage(true);
     } catch (error) {
       errorHandle(error.response.data.error_message);
+    } finally {
       isSubmit.value = false;
     }
   };
 
   const errorHandle = err => {
-    for (const key in err) {
-      errorMessage[key] = err[key];
-    }
+    errorMessage.value = err;
   };
 
   return { isSubmit, submitRegister, errorMessage };
