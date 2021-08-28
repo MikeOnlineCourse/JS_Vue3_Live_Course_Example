f0d4fa1b17c14ed4f77093879c0bf111a0ca5e92
Mike <a3804430@hotmail.com>
2021-08-28 20:52:19 +0800

    fix name

--- 03-Vue_VS_React/vue3-login-page/src/composition-api/useRegistered.js
+++ 03-Vue_VS_React/vue3-login-page/src/composition-api/useRegistered.js
@@ -1,17 +1,17 @@
 import { ref, reactive, inject } from 'vue';
 import axios from 'axios';
 export function useRegistered() {
-  const { handleReg } = inject('mapStore');
+  const { handleRegisterPage } = inject('mapStore');
   const isSubmit = ref(false);
   const errorMessage = reactive({});
 
-  const submitLogin = async registered => {
+  const submitRegister = async registered => {
     if (isSubmit.value) return;
     isSubmit.value = true;
     try {
       await axios.post('https://vue-lessons-api.herokuapp.com/auth/registered', registered);
       isSubmit.value = false;
-      handleReg(true);
+      handleRegisterPage(true);
     } catch (error) {
       errorHandle(error.response.data.error_message);
       isSubmit.value = false;
@@ -19,8 +19,10 @@ export function useRegistered() {
   };
 
   const errorHandle = err => {
-    Object.keys(err).forEach(key => (errorMessage[key] = err[key]));
+    for (const key in err) {
+      errorMessage[key] = err[key];
+    }
   };
 
-  return { isSubmit, submitLogin, errorMessage };
+  return { isSubmit, submitRegister, errorMessage };
 }
--- 03-Vue_VS_React/vue3-login-page/src/view/Home.vue
+++ 03-Vue_VS_React/vue3-login-page/src/view/Home.vue
@@ -1,18 +1,18 @@
 <script>
   import { inject } from 'vue';
-  import LoginPage from '../components/LoginPage.vue';
+  import RegisterPage from '../components/RegisterPage.vue';
   export default {
     components: {
-      LoginPage,
+      RegisterPage,
     },
     setup() {
-      const { isReg } = inject('mapStore');
-      return { isReg };
+      const { isRegister } = inject('mapStore');
+      return { isRegister };
     },
   };
 </script>
 <template>
-  <LoginPage v-if="!isReg" />
+  <RegisterPage v-if="!isRegister" />
   <div v-else>
     <h1>註冊成功</h1>
   </div>
